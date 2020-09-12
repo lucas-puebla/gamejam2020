03912ded7d66ee0f8d62f438ae7c596e997916a4
Lucas Puebla <luigilucas@gmail.com>
2020-09-12 02:19:22 -0300

    ADD Dash

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,29 +4,49 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
 	private Rigidbody2D rb;
 
-	public float speed;
-
+	// Movement
 	private float velX;
 	private float velY;
-
 	private Vector2 movement;
 
+	public float speed = 1f;
+
+	// Dash
+	private Timer dashCooldownTimer;
+	public float dashCooldownTime = 2f;
+
+	private Timer dashTimer;
+	public float dashTime = 0.15f;
+	public float dashSpeed = 5f;
 
     // Start is called before the first frame update
     void Start()
     {
+    	dashTimer = new Timer(dashTime);
+		dashCooldownTimer = new Timer(dashCooldownTime);
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update() {
-    	velX = Input.GetAxisRaw("Horizontal") * speed;
-        velY = Input.GetAxisRaw("Vertical") * speed;
-    	//Debug.Log("hor: " + velX + "\nver: " + velY);
+    	if (Input.GetButtonDown("Dash") && !dashCooldownTimer.isEnabled()) {
+    		dashTimer.reset();
+    		dashCooldownTimer.reset();
+    		// TODO when entering dash mode, cannot be damaged
+    	}
+
+    	if (dashTimer.isEnabled()) {
+    		setSpeed(dashSpeed);
+    	} else {
+    		setSpeed(speed);
+    	}
 
         movement = new Vector2(velX, velY);
+
+        // Countdowns
+        dashTimer.countDown();
+        dashCooldownTimer.countDown();
     }
 
     // Update is called once per frame
@@ -35,5 +55,8 @@ public class PlayerMovement : MonoBehaviour
        	rb.velocity = movement;
     }
 
-
+    void setSpeed(float speed) {
+    	velX = Input.GetAxisRaw("Horizontal") * speed;
+        velY = Input.GetAxisRaw("Vertical") * speed;
+    }
 }
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer {
+    private bool state;
+    private float maxTime;
+    private float remainingTime;
+
+    public Timer(float maxTime) {
+        state = false;
+        this.maxTime = maxTime;
+    }
+
+    public void reset() {
+        state = true;
+        remainingTime = maxTime;
+    }
+    
+    public void disable() {
+        state = false;
+    }
+
+    // Toggle use of countDown for update or fixedUpdate
+    // Do not use fixedUpdate on Update and vice versa
+    public void countDown(bool fixedUpdate = false) {
+        if (state) {
+            float time = fixedUpdate ? Time.fixedDeltaTime : Time.deltaTime;
+            remainingTime -= time;
+        }
+
+        if (state && remainingTime <= 0) {
+            disable();
+        }
+    }
+
+    public bool isEnabled() {
+        return state;
+    }
+
+    public void setMaxTime(float maxTime) {
+        this.maxTime = maxTime;
+    }
+
+    public float getMaxTime() {
+        return maxTime;
+    }
+
+    public float getRemTime() {
+        return remainingTime;
+    } 
+}
