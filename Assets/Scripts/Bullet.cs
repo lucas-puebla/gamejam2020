ad2fbb9131a8a76f540e71ca9f6ae729c7900b03
Lucas Puebla <luigilucas@gmail.com>
2020-09-12 22:52:55 -0300

    ADD Shooting, with explosions

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+	public GameObject hitEffect;
+	public float effectLifeTime = 0.3f;
+
+    void OnCollisionEnter2D(Collision2D other) {
+    	GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+    	Destroy(effect, effectLifeTime);
+    	Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/WeaponShoot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponShoot : MonoBehaviour
+{
+
+	public Transform firePoint;
+	public GameObject bulletPrefab;
+
+	public float bulletForce = 1f;
+
+	private Rigidbody2D rb;
+	private GameObject bullet;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) {
+        	Shoot();
+        }
+    }
+
+    void Shoot() {
+    	bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    	rb = bullet.GetComponent<Rigidbody2D>();
+    	rb.AddForce(firePoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+}
