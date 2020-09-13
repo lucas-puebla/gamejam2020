025a44496ac35dd40c3228310fda21ae305505e9
Lucas Puebla <luigilucas@gmail.com>
2020-09-13 17:15:41 -0300

    ADD music

---
+++ Assets/Music.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+
+	private AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
